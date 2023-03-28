b3a2951e919523bcd809a6bb902f6e013f0c8ae9
Lyara <lyara.sousa@sete.inf.br>
2023-03-28 13:31:27 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portfolioweb
+# portifolioweb
---
+++ script.js
@@ -0,0 +1,15 @@
+// var myAudio = document.getElementById('myAudio');
+// var player = new MediaElementPlayer(myAudio);
+
+
+// var player = new MediaElementPlayer(myAudio, {
+//   features: ['playpause', 'progress', 'current', 'duration', 'tracks', 'fullscreen'],
+//   audioVolume: 'vertical',
+//   audioWidth: '100%',
+//   audioHeight: 50,
+//   startVolume: 0.8,
+//   loop: false,
+//   alwaysShowControls: false,
+//   hideVolumeOnTouchDevices: true
+// });
+
